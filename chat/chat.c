de12ef22c7f52f8a8023daa37952634e23d14686
Eric Nelson <nelseric@mail.gvsu.edu>
2012-02-26 21:49:28 -0500

    have the chat CLI almost done using ncurses to make it nice

---
+++ chat/chat.c
@@ -0,0 +1,61 @@
+#define _XOPEN_SOURCE 500
+
+#include <stdlib.h>
+#include <curses.h>
+
+#include <pthread.h>
+
+#include <errno.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <unistd.h>
+
+#include "chat.h"
+
+void usage(char *argv[]) {
+    
+} 
+
+int main(int argc, char *argv[]){
+    if(argc < 3 || argc > 4){
+        fprintf(stderr, "Usage: %s <username> <host> (<port>)\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    //char *host = argv[2];
+    char *username = argv[1];
+    //set up connection
+    //get motd
+    
+    initscr();
+
+    printw("Hello %s", username);
+    getch();
+
+    endwin();
+}
+
+void cbuf_init(struct chat_buffer *cbuf){
+    cbuf->bsize = CBUF_SIZE_DEFAULT;
+    cbuf->msgbuf = malloc(sizeof(char *) * cbuf->bsize);
+    cbuf->nmsg = 0;
+    pthread_mutex_init(&cbuf->lock, NULL);
+}
+void cbuf_destroy(struct chat_buffer *cbuf){
+    pthread_mutex_lock(&cbuf->lock);
+    for(int i = 0; i < cbuf->nmsg; i++){
+        free(cbuf->msgbuf[i]);
+    }
+    free(cbuf->msgbuf);
+    pthread_mutex_unlock(&cbuf->lock);
+    pthread_mutex_destroy(&cbuf->lock);
+}
+
+void cbuf_add(struct chat_buffer *cbuf, const char *msg){
+    pthread_mutex_lock(&cbuf->lock);
+
+    pthread_mutex_unlock(&cbuf->lock);
+}
+
---
+++ chat/chat.h
@@ -0,0 +1,25 @@
+#ifndef __CHAT_H__
+#define __CHAT_H__
+
+#include <pthread.h>
+
+#define CBUF_SIZE_DEFAULT 20;
+
+struct chat_buffer{
+	int bsize;
+	int nmsg;
+	char **msgbuf; 
+	pthread_mutex_t lock;
+} chat_buffer;
+
+void cbuf_init(struct chat_buffer *cbuf);
+void cbuf_destroy(struct chat_buffer *cbuf);
+void cbuf_add(struct chat_buffer *cbuf, const char *msg);
+
+
+
+
+
+
+
+#endif
